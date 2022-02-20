c4c106923b97a1822ed8c8e89ca7fc8e921b431c
Gopal <68118204+gopalsays108@users.noreply.github.com>
2022-02-21 01:29:04 +0530

    Update and rename Topic 1. Analysis of Algorithm to Topic 1. Analysis of Algorithm.java

---
+++ week 1/Topic 1. Analysis of Algorithm.java
@@ -0,0 +1,32 @@
+/*
+From Kunal Kushawaha YT video till 59 mins
+#What is TC?
+ TC is not the time taken by the machine, instead it is the function that gives us a reltioanship about how the time will grow as the input grows.
+ Same algo can take different timee in different machine.
+
+#What do we need to consider when thining of TC.
+ 1. Always look for the worst case complexity.
+ 2. Always look for complexity for larger number.
+ 3. We ignore the constant.
+ 4. We ignore the less domntating term.
+ 
+#Big O notation
+ It is an upper bound of an algo. for ex if given Big O = N^3, means the graph/relationaship can not exceed N^3 . 
+ it maybe solved in N, log n, N^2 but not abve given complexity
+ 
+#Big Omega notation.
+ It is the opposite of Big O
+ It give lower bound of an algo
+ Eg: given omega N^3 means algo will take min N^3 time or more than it 
+ 
+#Big theta notation.
+ See in textbook
+ 
+#little o notation
+ see in text
+#little omega 
+ see intext
+ 
+#space compexity
+ Input space + auxillary space
+*/

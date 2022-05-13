38cea7b61d6c52b52fa83fabccdd76405ad317ee
Gopal <68118204+gopalsays108@users.noreply.github.com>
2022-05-13 22:31:39 +0530

    Update 9. Backspace String Compare.java

--- codes/leetcode/9. Backspace String Compare.java
+++ codes/leetcode/9. Backspace String Compare.java
@@ -47,3 +47,46 @@ class Solution {
         return true;
     }
 }
+
+/*Stack Solution*/
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> st = new Stack<>();
+        Stack<Character> st2 = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '#'){
+                if(!st.isEmpty()) st.pop();
+            }else{
+                st.push(s.charAt(i));
+            }
+        }
+        
+        
+        for(int i = 0; i < t.length(); i++){
+            if(t.charAt(i) == '#'){
+                if(!st2.isEmpty()) st2.pop();
+            }else{
+                st2.push(t.charAt(i));
+            }
+        } 
+        System.out.println(st + "  " + st2);
+        StringBuilder sb = new StringBuilder();
+        
+        StringBuilder sb2 = new StringBuilder();
+        
+        
+        while(!st2.isEmpty()){
+                sb2.append(st2.pop());
+        }
+        
+        
+        while(!st.isEmpty()){
+                sb.append(st.pop());
+        }
+        System.out.println(sb + " " + sb2);
+        return sb.toString().equals( sb2.toString());
+        
+    }
+}
+

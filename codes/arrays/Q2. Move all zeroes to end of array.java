d4ded0126f9c18f91e474479aa3a00940239c6f4
Gopal <68118204+gopalsays108@users.noreply.github.com>
2022-05-25 23:45:15 +0530

    Update Q2. Move all zeroes to end of array.java

--- codes/arrays/Q2. Move all zeroes to end of array.java
+++ codes/arrays/Q2. Move all zeroes to end of array.java
@@ -5,7 +5,7 @@ class Solution {
     */
     void pushZerosToEnd(int[] arr, int n) {
         // code here
-        int count = 0;
+        int count  = 0;
         for(int i = 0; i<n;i++){
             if(arr[i] != 0)
                 swap(arr,count++,i);
